cd80a3978bc27272c3ac1862893ca7d91dd6222d
Hoai Phuoc Truong <hptruong93@users.noreply.github.com>
2016-12-28 01:47:22 -0500

    Create hw1.py

---
+++ hw1.py
@@ -0,0 +1,21 @@
+# print out numbers from 1 to 100
+def simple_starter():
+    pass
+    
+# print out numbers from 1 to 100. if that number is divisible by both 3 and 5, print out "FizzBuzz". if that number is divisible by 3 only,
+# print out "Fizz". If that number is divisible by 5 only, print out "Buzz". Otherwise print out the number as usual.
+def fizzbuzz():
+    pass
+
+# write a function to check if number is prime or not
+def is_prime(n):
+    pass
+
+# write a function to reverse an array
+def do_reverse(array):
+    pass
+
+# write a function to check if a string is palindrome. A string is palindrome if its reverse is identical. Example: "racecar", "goodoog"
+def is_palindrome(string):
+    pass
+    
