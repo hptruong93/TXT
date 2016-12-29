f199823f2eaf192b144fb7fea87ac289f9f08138
Hoai Phuoc Truong <hptruong93@users.noreply.github.com>
2016-12-28 22:08:07 -0500

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -41,3 +41,28 @@ def reverse_list(head_node):
 # Example: merge([1,5,7], [3,6,7]) should produce [1,3,5,6,7,7]
 def merge(arr1, arr2):
    pass
+
+
+# Implement a stack with a link list.
+head_node = Node(1)
+
+# Push method modifies the linked list so that a new value is put at the head of the linked list.
+# The method returns a new linked list whose top value is the new top value in the stack.
+def stack_push(head, value):
+    if head is None:
+        return Node(value)
+    
+    new_head = Node(value)
+    new_head.next = head
+    return new_head
+
+# Pop method modifies the linked list so that the value at the head of the list is removed.
+# The method returns a new linked list whose top value is the new top value in the stack.
+# If the list has only 1 element (and so is the stack), return None as there is nothing left in the stack.
+# No need to take care of the case where we pop from an empty stack.
+def stack_pop(head):
+    pass
+
+# Peek method does not modify the linked list. It returns the value of the node at the head of the list (i.e. the top of stack).
+def stack_peek(head, value):
+    pass
