b01f9b01ac495dd17a98708d59a54e824b63ffe6
Hoai Phuoc Truong <hptruong93@users.noreply.github.com>
2017-01-10 09:11:05 -0500

    Create number_to_text.py

---
+++ number_to_text.py
@@ -0,0 +1,9 @@
+
+
+def number_to_text(number):
+  pass
+
+if __name__ == "__main__":
+  numbers = [1, 2, 7, 10, 14, 25, 37, 48, 59, 99, 101, 134, 157, 587, 10547, 4927854]
+  for number in numbers:
+    print(number_to_text(number))
