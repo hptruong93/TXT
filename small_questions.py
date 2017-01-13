c31a780a20c90eda1d06e0c544691496454de2fc
Hoai Phuoc Truong <hptruong93@users.noreply.github.com>
2017-01-13 07:37:49 -0500

    Update small_questions.py

--- small_questions.py
+++ small_questions.py
@@ -1,9 +1,28 @@
 # Invert a binary tree
 
-class Node:
+class TreeNode:
   def __init__(self, value):
     self.value = value
-    self.next = None
+    self.left = None
+    self.right = None
     
+one = TreeNode(1)
+two = TreeNode(2)
+three = TreeNode(3)
+four = TreeNode(4)
+five = TreeNode(5)
+six = TreeNode(6)
+seven = TreeNode(7)
+
+root_node = four
+four.left = two
+two.left = one
+two.right = three
+
+four.right = six
+six.left = five
+six.right = seven
+
+  
 def invert_binary_tree(root):
   pass
